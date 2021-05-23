557a45c5acf06cad7c111c91cd883dc18341825e
Tomas <tomas.v.mob@gmail.com>
2021-05-23 12:41:31 +0300

    Packages #28

--- src/toVaPro/ND0520/Maps_Experiments.java
+++ src/toVaPro/ND0520/Maps_Experiments.java
@@ -14,29 +14,50 @@ public class Maps_Experiments {
     public static void main(String[] args) {
 
         String st;
-        
+
         Map m1 = new HashMap();
         Map m2 = new HashMap();
-        
+        Map m3 = new HashMap();
+
         m1.put(5, "labas, labas, labas");
         System.out.println(m1);
         m1.put(7, "pasauli, pasauli, pasauli");
         System.out.println(m1);
-        
+
         System.out.println("******** ******** ******** ********\n");
-        
-        st ="";
+
+        st = "";
         st += "labas";
         st += ", labas";
         st += ", labas";
         m2.put(5, st);
         System.out.println(m2);
-        
-        st ="";
+
+        st = "";
         st += "pasauli";
         st += ", pasauli";
         st += ", pasauli";
         m2.put(7, st);
         System.out.println(m2);
+
+        System.out.println("******** ******** ******** ********\n");
+
+        List l = new ArrayList();
+
+        l.add("labas");
+        l.add("labas");
+        l.add("labas");
+        m3.put(5, l);
+        System.out.println(m3);
+
+//        l.removeAll(l);
+//        l.clear();
+        l = new ArrayList();
+        l.add("pasauli");
+        l.add("pasauli");
+        l.add("pasauli");
+        m3.put(7, l);
+        System.out.println(m3);
+        
     }
 }
